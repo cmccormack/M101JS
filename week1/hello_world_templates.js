b77b1beee188ff9d600f5e7dc6dfbc2a37ee0f55
cmccormack <mccormack.christopher@gmail.com>
2017-10-19 15:50:52 -0700

    Initial Commit

---
+++ week1/hello_world_templates.js
@@ -0,0 +1,16 @@
+const app = require('express')()
+
+app.get('/', (req, res)=>{
+	res.type('text')
+	res.status(200)
+	res.send("Hello, World!")
+})
+
+app.use((req, res)=>{
+	res.sendStatus(404)
+})
+
+var server = app.listen(3000, ()=>{
+	const port = server.address().port
+	console.log('Express server listening on port %s', port)
+})
