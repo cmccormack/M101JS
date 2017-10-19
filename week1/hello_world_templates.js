9c30a0e5e8bf1ad8dea36c87398a9fb435db62e6
cmccormack <mccormack.christopher@gmail.com>
2017-10-19 16:51:52 -0700

    Added engines for templates

--- week1/hello_world_templates.js
+++ week1/hello_world_templates.js
@@ -1,9 +1,19 @@
 const app = require('express')()
+const engines = require('consolidate')
+const path = require('path')
+
+// Map the nunjucks template engine to ".html" files
+app.engine('html', engines.nunjucks)
+
+// The default engine extension to use when omitted
+app.set('view engine', 'html')
+
+// A directory or an array of directories for the application's views.
+// If an array, the views are looked up in the order they occur in the array.
+app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', (req, res)=>{
-	res.type('text')
-	res.status(200)
-	res.send("Hello, World!")
+	res.render('hello', {'name': 'World Templates!'})
 })
 
 app.use((req, res)=>{
